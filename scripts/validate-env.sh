d8a9118e754a03e94536dde529633fb1f77895f4
Madhavankarthik <madhavankarthi2010@gmail.com>
2023-07-04 10:01:56 +0530

    Added logic to fail the job on failure

--- scripts/validate-env.sh
+++ scripts/validate-env.sh
@@ -11,13 +11,12 @@ get_env_var_names() {
 
 compare_env_vars() {
   env_vars1=$(get_env_var_names "$JOB_1_NAME")
-  echo "$env_vars1"
   env_vars2=$(get_env_var_names "$JOB_2_NAME")
-  echo "$env_vars2"
   if [[ "$env_vars1" == "$env_vars2" ]]; then
     echo "Environment variables names for $JOB_1_NAME and $JOB_2_NAME are the same."
   else
     echo "Environment variables names for $JOB_1_NAME and $JOB_2_NAME are different."
+    exit 1
   fi
 }
 
