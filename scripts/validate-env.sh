6f431f5fb4de1024881f91deb3ae3602c5cee051
Madhavankarthik <madhavankarthi2010@gmail.com>
2023-07-04 10:07:12 +0530

    Added additional info on error scenario

--- scripts/validate-env.sh
+++ scripts/validate-env.sh
@@ -4,7 +4,6 @@ FILE_NAME=${1}
 JOB_1_NAME=${2}
 JOB_2_NAME=${3}
 
-# Parse YAML file and extract environment variables
 get_env_var_names() {
   yq eval '.jobs.'$1'.environment[] | keys' "$FILE_NAME"
 }
@@ -16,8 +15,8 @@ compare_env_vars() {
     echo "Environment variables names for $JOB_1_NAME and $JOB_2_NAME are the same."
   else
     echo "Environment variables names for $JOB_1_NAME and $JOB_2_NAME are different."
-    echo "Environment variables names for $JOB_1_NAME: $env_vars1"
-    echo "Environment variables names for $JOB_2_NAME: $env_vars2"
+    echo "Environment variables names for $JOB_1_NAME: \n$env_vars1"
+    echo "Environment variables names for $JOB_2_NAME: \n$env_vars2"
     exit 1
   fi
 }
